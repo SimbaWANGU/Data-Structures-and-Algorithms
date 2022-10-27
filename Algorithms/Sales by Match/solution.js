757524563ae943afe55def9e04f52eb377bb8fcb
SimbaWANGU <simbawangulu@gmail.com>
2022-10-27 12:14:22 +0300

    Add sales by match algorithm description and solution

---
+++ Algorithms/Sales by Match/solution.js
@@ -0,0 +1,31 @@
+"use strict";
+function salesByMatch(arr) {
+    let pairs = 0;
+    let i = 0;
+    let prevColor;
+    let currColor;
+    let sorted = arr.sort((a, b) => a - b);
+    for (i; i < sorted.length; i++) {
+        if (sorted[i + 1]) {
+            prevColor = sorted[i];
+            currColor = sorted[i + 1];
+            if (prevColor === currColor) {
+                pairs++;
+                sorted.shift();
+                sorted.shift();
+                i--;
+            }
+            else {
+                sorted.shift();
+                i--;
+            }
+        }
+        else {
+            return pairs;
+        }
+    }
+    return pairs;
+}
+console.log(salesByMatch([1, 2, 1, 2, 1, 3, 2])); //=> 2
+console.log(salesByMatch([10, 20, 20, 10, 10, 30, 50, 10, 20])); //=> 3
+console.log(salesByMatch([1, 1, 3, 1, 2, 1, 3, 3, 3, 3])); //=> 4
